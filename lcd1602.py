c1539051257abf378f2b246fa61301c128eb1616
wrabaa <141175554+wrabaa@users.noreply.github.com>
2023-08-01 23:20:56 +0300

    lcd1602.py

--- lcd1602.py
+++ lcd1602.py
@@ -5,14 +5,17 @@ import time
 lcd = CharLCD('PCF8574', 0x27)
 
 try:
-    # Display "Hello, World!" on the LCD
-    lcd.write_string("Hello, World!")
+    # Display "Hello" on the first line of the LCD
+    lcd.cursor_pos = (0, 0)
+    lcd.write_string("Hello")
 
-    # Wait for 5 seconds
-    time.sleep(5)
+    # Display "World!" on the second line of the LCD
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string("World!")
 
-    # Clear the LCD
-    lcd.clear()
+    # Keep the message displayed until interrupted by the user
+    while True:
+        pass
 
 except KeyboardInterrupt:
     # Clear the LCD before exiting
